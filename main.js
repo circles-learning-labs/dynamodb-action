c4ef650d4f2cdc4f7955b83b0d2be35fc5850e56
Bernard Duggan <bernard@circl.es>
2021-03-25 17:38:16 +1100

    Try this then

--- main.js
+++ main.js
@@ -16,7 +16,5 @@ const settings = ["port", "dbPath", "sharedDb", "cors", "delayTransientStatuses"
 		settings.version ? settings.version : "latest"
 	].filter((a) => Boolean(a)).join(" ");
 
-	// Pin version to 1.13.6 - 1.15 (the latest at time of wriging) contains a bug where the error
-	// message has the key "Message" rather than "message", which breaks ExAws's error parsing.
 	await exec.exec(`sudo docker run --name dynamodb -d -p ${settings.port}:${settings.port} amazon/dynamodb-local:${settings.version} -jar DynamoDBLocal.jar -port ${settings.port}${extraArguments ? ` ${extraArguments}` : ""}`);
 })();
